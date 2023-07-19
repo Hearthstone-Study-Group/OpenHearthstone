4f0db6e9cc37a1db2e564ee98d2c417c40df5cde
As Cold as Ice <bxymartin@hotmail.com>
2023-07-19 16:16:27 +0000

    Fixed bugs in data loader and add descriptions

--- Baseline/test/test_loader.py
+++ Baseline/test/test_loader.py
@@ -1,5 +1,6 @@
 import unittest
 from data.loader import DataLoader
+from data.transition import TransitionLoader
 from transformers import GPT2Tokenizer
 import numpy as np
 
@@ -16,6 +17,20 @@ class LoaderTestCase(unittest.TestCase):
         print("state length max", np.max(source_data[:, 0]), "min", np.min(source_data[:, 0]))
         print("action length max", np.max(source_data[:, 1]), "min", np.min(source_data[:, 1]))
         print("option length max", np.max(source_data[:, 2]), "min", np.min(source_data[:, 2]))
+
+
+    def test_transition(self):
+        folder_path = "../storage/v0.1"
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        data_loader = TransitionLoader(folder_path, tokenizer)
+        data_loader.max_length = 1e9
+        training_data_property = data_loader.check_data_loader()
+        source_data = np.array(training_data_property)
+        print()
+        print("state length max", np.max(source_data[:, 0]), "min", np.min(source_data[:, 0]))
+        print("action length max", np.max(source_data[:, 1]), "min", np.min(source_data[:, 1]))
+        print("next action length max", np.max(source_data[:, 2]), "min", np.min(source_data[:, 2]))
+
 
 
 if __name__ == '__main__':
